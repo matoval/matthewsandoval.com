8043994b8b239cf67d9e379a5eb5d3fd5aa82597
Matthew Sandoval <mat24c@gmail.com>
2020-11-11 16:17:59 -0800

    fixed lighthouse score and seo

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
 function dropdown() {
-  let isMenuOpen = false
-  const mobileNavDisplay = document.getElementById('mobile-nav')
-  const menuBtn = document.getElementById('menu-btn')
+  let isMenuOpen = false;
+  const mobileNavDisplay = document.getElementById("mobile-nav");
+  const menuBtn = document.getElementById("menu-btn");
 
-  menuBtn.addEventListener('click', () => {
-    isMenuOpen = !isMenuOpen
+  menuBtn.addEventListener("click", () => {
+    isMenuOpen = !isMenuOpen;
     if (isMenuOpen) {
-      mobileNavDisplay.style.display = 'block'
-      menuBtn.innerHTML = 'X'
+      mobileNavDisplay.style.display = "block";
+      menuBtn.innerHTML = "X";
     } else {
-      mobileNavDisplay.style.display = 'none'
-      menuBtn.innerHTML = '&#9776;'
+      mobileNavDisplay.style.display = "none";
+      menuBtn.innerHTML = "&#9776;";
     }
-    
-  })
+  });
 }
 
 function toggleDarkMode(event) {
-  console.log(event.target.checked)
-  const isDarkmode = event.target.checked
+  console.log(event.target.checked);
+  const isDarkmode = event.target.checked;
 
   if (isDarkmode) {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    document.getElementById('slider').classList.add('dark')
+    document.documentElement.setAttribute("data-theme", "dark");
+    document.getElementById("slider").classList.add("dark");
   } else {
-    document.documentElement.setAttribute('data-theme', '')
-    document.getElementById('slider').classList.remove('dark')
+    document.documentElement.setAttribute("data-theme", "");
+    document.getElementById("slider").classList.remove("dark");
   }
 }
 
 async function getArticle() {
-  const response = await fetch('https://dev.to/search/feed_content?per_page=15&page=0&user_id=297524&class_name=Article&sort_by=published_at&sort_direction=desc&approved=')
-  const data = await response.json()
-  const articles = data.result
+  const response = await fetch(
+    "https://dev.to/search/feed_content?per_page=15&page=0&user_id=297524&class_name=Article&sort_by=published_at&sort_direction=desc&approved="
+  );
+  const data = await response.json();
+  const articles = data.result;
 
   for (const article of articles) {
-    document.getElementById('article-posts').insertAdjacentHTML('beforeend', `<div class='article'><a href='https://dev.to${article.path}' target='_blank'><img src='${article.main_image}'/><br/><h2>${article.title}</h2></a></div>`)
+    document
+      .getElementById("article-posts")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<div class='article'><a href='https://dev.to${article.path}' target='_blank' rel="noopener" rel="noreferrer"><img src='${article.main_image}' alt="article banner"/><br/><h2>${article.title}</h2></a></div>`
+      );
   }
 }
 
-dropdown()
-getArticle()
+dropdown();
+getArticle();
