505ab3febfdcadaf8a39eac58c3640c8e94d1850
matoval <mat24c@gmail.com>
2023-09-24 19:20:32 -0700

    Remove tailwind

--- src/app/components/mdx-card.tsx
+++ src/app/components/mdx-card.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link"
 
-import { cn } from "../../lib/utils"
-
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   href?: string
   disabled?: boolean
@@ -16,11 +14,6 @@ export function MdxCard({
 }: CardProps) {
   return (
     <div
-      className={cn(
-        "group relative rounded-lg border p-6 shadow-md transition-shadow hover:shadow-lg",
-        disabled && "cursor-not-allowed opacity-60",
-        className
-      )}
       {...props}
     >
       <div className="flex flex-col justify-between space-y-4">
--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,9 +1,3 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 export function formatDate(input: string | number): string {
   const date = new Date(input)
